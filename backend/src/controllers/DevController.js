88ffb6e4afc37d8c18cae1d832cf5f0caf4f6cde
Roger Brusa <roger@sbsistemas.com.br>
2020-01-14 13:52:38 -0300

    Parse, Map, Controller

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,7 +1,15 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
-
+const parseStringAsArray = require('../utils/parseStringAsArray');
 module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
+
+
   async store(request, response) {
     const { github_username, techs, latitude, longitude } = request.body;
 
@@ -10,7 +18,7 @@ module.exports = {
     if (!dev) {
       const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
       const { name = login, avatar_url, bio } = apiResponse.data;
-      const techsArray = techs.split(',').map(techs => techs.trim());
+      const techsArray = parseStringAsArray(techs);
 
       const location = {
         type: 'Point',
