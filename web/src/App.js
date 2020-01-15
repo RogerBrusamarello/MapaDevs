a1a39f59aa608f3ba9235bafed2b075b6c16cf50
Roger Brusa <roger@sbsistemas.com.br>
2020-01-15 09:23:07 -0300

    css

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,8 @@
 import React from 'react';
 import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
@@ -11,11 +13,71 @@ function App() {
       <aside>
         <strong>Cadastrar</strong>
         <form>
-          <label htmlFor=""></label>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
         </form>
       </aside>
-      <main>
 
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/34141109?s=460&v=4" alt="Imagem" />
+              <div className="user-info">Roger Brusamarello</div>
+              <span>ReactJS, Flutter</span>
+            </header>
+            <p>Acadêmico em Análise e Desenvolvimento de Sistema pela Universidade de Passo Fundo. Desenvolvedor Mobile. Desenvolvedor Front-End.</p>
+            <a href="https://github.com/rogerbrusamarello">Acessar perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/34141109?s=460&v=4" alt="Imagem" />
+              <div className="user-info">Roger Brusamarello</div>
+              <span>ReactJS, Flutter</span>
+            </header>
+            <p>Acadêmico em Análise e Desenvolvimento de Sistema pela Universidade de Passo Fundo. Desenvolvedor Mobile. Desenvolvedor Front-End.</p>
+            <a href="https://github.com/rogerbrusamarello">Acessar perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/34141109?s=460&v=4" alt="Imagem" />
+              <div className="user-info">Roger Brusamarello</div>
+              <span>ReactJS, Flutter</span>
+            </header>
+            <p>Acadêmico em Análise e Desenvolvimento de Sistema pela Universidade de Passo Fundo. Desenvolvedor Mobile. Desenvolvedor Front-End.</p>
+            <a href="https://github.com/rogerbrusamarello">Acessar perfil no GitHub</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/34141109?s=460&v=4" alt="Imagem" />
+              <div className="user-info">Roger Brusamarello</div>
+              <span>ReactJS, Flutter</span>
+            </header>
+            <p>Acadêmico em Análise e Desenvolvimento de Sistema pela Universidade de Passo Fundo. Desenvolvedor Mobile. Desenvolvedor Front-End.</p>
+            <a href="https://github.com/rogerbrusamarello">Acessar perfil no GitHub</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
